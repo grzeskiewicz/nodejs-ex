a3c59c358580be7c6c53f12a98b846fe92080761
grzeskiewicz <ariel.grzeskiewicz@gmail.com>
2018-11-13 19:24:01 +0100

    Changing routes

---
+++ routes/routes.js
@@ -0,0 +1,59 @@
+var express = require('express'),
+    app     = express();
+var cors = require('cors');
+
+var bodyParser = require('body-parser');
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+app.use(bodyParser.json()); // support json encoded bodies 
+
+var db = require('./database/database');
+var user = require('./usercontroller/usercontroller');
+var upload= require('./upload/upload');
+
+
+app.options('*', cors()) // include before other routes
+
+app.get('/', function (req, res) {
+res.send('Hello');
+});
+
+
+
+
+app.get('/showings', db.showings);
+app.get('/showingsbydate/:date', db.showingsbydate);
+app.get('/seatsshowing/:showingid', db.seatsshowing);
+app.get('/seatstaken/:showingid', db.seatstaken);
+
+app.post('/newticket',db.newticket);
+app.post('/newshowing',db.newshowing);
+app.post('/newfilm',db.newfilm);
+app.post('/newprice',db.newprice);
+
+app.post('/deleteshowing',db.deleteshowing);
+app.post('/deletefilm',db.deletefilm);
+app.post('/deleteprice',db.deleteprice);
+app.post('/deleteticket',db.deleteticket);
+
+app.post('/editfilm',db.editfilm);
+app.post('/editcustomer',db.editcustomer);
+
+app.get('/tickets', db.ticketsquery);
+app.get('/rooms', db.roomsquery);
+app.get('/prices', db.pricesquery);
+app.get('/films', db.filmsquery);
+app.get('/showingsquery', db.showingsquery);
+app.post('/ticketsbycustomer',db.ticketsbycustomer);
+//app.post('/upload',upload.upload);
+
+app.get('/sign-s3', upload.signin);
+
+app.get('/test', db.test);
+//app.route('/newticket').post(user.loginRequired, db.newticket);
+
+app.post('/registertest', user.register);
+app.post('/logintest', user.login);
+app.get('/memberinfo', user.memberinfo);
+app.get('/customers',user.customers);
+app.post('/deletecustomer',user.deletecustomer);
--- server.js
+++ server.js
@@ -1,22 +1,22 @@
-//  OpenShift sample Node application
 var express = require('express'),
     app     = express();
 var cors = require('cors');
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 var bodyParser = require('body-parser');
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 app.use(bodyParser.json()); // support json encoded bodies 
-//var io = require('socket.io')(http);
-var db = require('./database/database');
-var user = require('./usercontroller/usercontroller');
 
 
-var upload= require('./upload/upload');
+
 const aws = require('aws-sdk');
 app.use(express.static('./public'));
 const S3_BUCKET = process.env.S3_BUCKET;
 aws.config.region = 'us-east-1';
+
+const routes = require('./routes/routes');
+app.use('/', routes);
 
 //var jwt = require('jsonwebtoken');    
 /*app.use(function(req, res, next) {
@@ -40,67 +40,24 @@ aws.config.region = 'us-east-1';
 
 
 
-app.options('*', cors()) // include before other routes
 
-app.get('/', function (req, res) {
-res.send('Hello');
-});
+/*
+io.on('connection', function(socket) {
+    socket.broadcast.emit('hi');
+    console.log('a user connected');
+    socket.on('disconnect', function() {
+        console.log('user disconnected');
+    });
 
-
-
-
-app.get('/showings', db.showings);
-app.get('/showingsbydate/:date', db.showingsbydate);
-app.get('/seatsshowing/:showingid', db.seatsshowing);
-app.get('/seatstaken/:showingid', db.seatstaken);
-
-app.post('/newticket',db.newticket);
-app.post('/newshowing',db.newshowing);
-app.post('/newfilm',db.newfilm);
-app.post('/newprice',db.newprice);
-
-app.post('/deleteshowing',db.deleteshowing);
-app.post('/deletefilm',db.deletefilm);
-app.post('/deleteprice',db.deleteprice);
-app.post('/deleteticket',db.deleteticket);
-
-app.post('/editfilm',db.editfilm);
-app.post('/editcustomer',db.editcustomer);
-
-app.get('/tickets', db.ticketsquery);
-app.get('/rooms', db.roomsquery);
-app.get('/prices', db.pricesquery);
-app.get('/films', db.filmsquery);
-app.get('/showingsquery', db.showingsquery);
-app.post('/ticketsbycustomer',db.ticketsbycustomer);
-//app.post('/upload',upload.upload);
-
-app.get('/sign-s3', upload.signin);
-
-app.get('/test', db.test);
-//app.route('/newticket').post(user.loginRequired, db.newticket);
-
-app.post('/registertest', user.register);
-app.post('/logintest', user.login);
-app.get('/memberinfo', user.memberinfo);
-app.get('/customers',user.customers);
-app.post('/deletecustomer',user.deletecustomer);
-
-app.get('/hehe', function(req, res, next) {
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end("Hello World lamusie!" + process.version);
-});
-
-
-
-
-// error handling
-/*app.use(function(err, req, res, next){
-  console.error(err.stack);
-  res.status(500).send('Something bad happened!');
+    socket.on('chat message', function(msg) {
+        console.log('message: ' + msg);
+        io.emit('chat message', msg);
+    });
 });*/
-var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
-    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+
+
+var port = process.env.PORT || 8080,
+    ip   = process.env.IP   || '0.0.0.0';
 
 http.listen(port, ip);
 console.log('Server running on http://%s:%s', ip, port);
